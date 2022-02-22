d75bd54971bb79ebdc9466e239ffc719bebd0fc9
Kemalcan <kemalcangnr47@gmail.com>
2022-02-22 19:43:09 +0300

    module1_solution

---
+++ app.js
@@ -0,0 +1,27 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+
+ function LunchCheckController($scope) {
+  $scope.name = "";
+  $scope.message = "";
+
+
+  $scope.sayMessage = function (){
+    const words = $scope.name.split(',');
+    var length = words.length;
+    console.log(length);
+    if (length <= 3 || $scope.name != ""){$scope.message = "Enjoy";}
+    if (length > 3){$scope.message = "That's too much";}
+    if ($scope.name == ""){$scope.message = "Please enter data";}
+  };
+
+};
+
+
+
+})();
